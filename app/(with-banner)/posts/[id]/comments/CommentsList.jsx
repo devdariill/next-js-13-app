7c5902712887fa007328dcb75a70a8ca05353faf
nator <ronaaldlll@gmail.com>
2022-11-02 22:28:40 -0500

    use react x fetch

---
+++ app/(with-banner)/posts/[id]/comments/CommentsList.jsx
@@ -0,0 +1,25 @@
+'use client';
+import {use} from 'react';
+// import Image from "next/image";
+const fetchComments = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  // throw new Error("Error en la API");
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
+    next: { revalidate: 60 },
+  }).then(res => res.json()); 
+};
+export function CommetsList({ id }) {
+  const commnets = use(fetchComments(id));  
+  return(
+    <ul style={ { borderRadius:'10px', background:'#444',fontSize:'10px' , }}>
+        {commnets.map(comment => (
+            <li key={comment.id} style={{padding:'10px '}}>
+              
+                <h3>{comment.name}</h3>
+                <p>{comment.body}</p>
+                <img width="50" height='50'alt={comment.name} src={`https://avatars.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`}  />
+            </li>
+        ))}
+    </ul>
+  );
+}
--- app/(with-banner)/posts/[id]/comments/page.jsx
+++ app/(with-banner)/posts/[id]/comments/page.jsx
@@ -1,25 +1,10 @@
-import Image from "next/image";
-
-const fetchComments = async (id) => {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  // throw new Error("Error en la API");
-  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
-    next: { revalidate: 60 },
-  }).then(res => res.json()); 
-};
-export default async function Post({ params }) {
+// import Image from "next/image";
+import {CommetsList} from './CommentsList.jsx';
+export default function Post({ params }) {
   const { id } = params;
-  const commnets = await fetchComments(id);  
-  return(
-    <ul style={ { borderRadius:'10px', background:'#444',fontSize:'10px' , }}>
-        {commnets.map(comment => (
-            <li key={comment.id} style={{padding:'10px '}}>
-              
-                <h3>{comment.name}</h3>
-                <p>{comment.body}</p>
-                <img width="50" height='50'alt={comment.name} src={`https://avatars.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`}  />
-            </li>
-        ))}
+  return (
+    <ul>
+      <CommetsList id={id} />
     </ul>
   );
 }
