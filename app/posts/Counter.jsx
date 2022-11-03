e520abe14d97c0f49d45b7cc77059f421d30305d
nator <ronaaldlll@gmail.com>
2022-11-02 19:18:16 -0500

    post<id>

--- app/posts/Counter.jsx
+++ app/posts/Counter.jsx
@@ -6,8 +6,8 @@ export function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <p>Count:{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>;
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,4 +1,19 @@
+const fetchSinglePost = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: { revalidate: 60 },
+  }).then(res => res.json()); 
+};
 export default async function PostsPage({ params }) {
-  const { id } = params;
-  return <h1>Post {id}</h1>;
+  const { id } = params; 
+  const post = await fetchSinglePost(id);  
+  return(
+  <>
+    <h1>Post {id}</h1>
+      <article key={post.id}>
+        <h2 style={{ color: "#09f" }}>{post.title}</h2>
+        <p>{post.body}</p>
+    </article>
+
+  </>
+  );
 }
